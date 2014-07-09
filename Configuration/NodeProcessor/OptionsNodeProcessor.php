82f54c59777ba4d870ab5fe15b7fc09448c3e903
Blake ETEME <blake.eteme@gmail.com>
2014-07-09 12:10:25 +0200

    Fix option resolving for route attr

--- Configuration/NodeProcessor/OptionsNodeProcessor.php
+++ Configuration/NodeProcessor/OptionsNodeProcessor.php
@@ -7,6 +7,7 @@ use Knp\Menu\ItemInterface;
 use Lephare\Bundle\MenuBundle\Configuration\ConfigurationPriorityList;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
 
 class OptionsNodeProcessor extends AbstractNodeProcessor implements NodeProcessorInterface
 {
@@ -25,31 +26,44 @@ class OptionsNodeProcessor extends AbstractNodeProcessor implements NodeProcesso
         $this->setDefaultOptions($resolver);
 
         $options = $resolver->resolve($configuration);
+        $options = $factory->createItem('node', $options)->toArray();
+
         $this->configureItem($node, $options);
     }
 
     protected function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults([
-            'uri' => null,
-            'label' => null,
-            'attributes' => array(),
-            'linkAttributes' => array(),
-            'childrenAttributes' => array(),
-            'labelAttributes' => array(),
-            'extras' => array(),
-            'display' => true,
-            'displayChildren' => true,
-            'route' => null,
-        ]);
+        $resolver
+            ->setDefaults([
+                'uri' => null,
+                'label' => null,
+                'attributes' => [],
+                'linkAttributes' => [],
+                'childrenAttributes' => [],
+                'labelAttributes' => [],
+                'display' => true,
+                'displayChildren' => true,
+                'route' => null,
+                'routeParameters' => [],
+                'extras' => [],
+            ])
+
+            ->setAllowedTypes([
+                'uri' => [ 'string', 'null' ],
+                'label' => [ 'string', 'null' ],
+                'attributes' => 'array',
+                'linkAttributes' => 'array',
+                'childrenAttributes' => 'array',
+                'labelAttributes' => 'array',
+                'display' => 'bool',
+                'displayChildren' => 'bool',
+                'route' => [ 'null', 'string' ],
+                'routeParameters' => 'array',
+                'extras' => 'array',
+            ])
+        ;
     }
 
-    /**
-     * Configures the newly created item with the passed options
-     *
-     * @param ItemInterface $item
-     * @param array         $options
-     */
     protected function configureItem(ItemInterface $item, array $options)
     {
         $item
